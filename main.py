f2be179d0730f207618d4fc09cf867d902e8b8ba
rey444 <shreya.sapkal959@gmail.com>
2023-05-27 23:15:13 -0700

    work.

--- main.py
+++ main.py
@@ -4,26 +4,29 @@ import threading
 from flask import render_template  # import render_template from "public" flask libraries
 
 # import "packages" from "this" project
-from __init__ import app  # Definitions initialization
+from __init__ import app, db  # Definitions initialization
 from model.jokes import initJokes
 from model.users import initUsers
 from model.carspecs import initCarSpec
+from model.readers import initReaders
 
 # setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
+from api.covid import covid_api  # Blueprint import api definition
+from api.joke import joke_api  # Blueprint import api definition
+from api.user import user_api  # Blueprint import api definition
 from api.carspec import carspec_api
+from api.reader import reader_api
 
 # setup App pages
-from projects.projects import app_projects # Blueprint directory import projects definition
+from projects.projects import app_projects  # Blueprint directory import projects definition
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
+app.register_blueprint(joke_api)  # register api routes
+app.register_blueprint(covid_api)  # register api routes
+app.register_blueprint(user_api)  # register api routes
 app.register_blueprint(carspec_api)
-app.register_blueprint(app_projects) # register app pages
+app.register_blueprint(reader_api)
+app.register_blueprint(app_projects)  # register app pages
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -38,14 +41,28 @@ def index():
 def stub():
     return render_template("stub.html")
 
-@app.before_first_request
+initialized = False  # Global flag for initialization
+
+def initialize_once():
+    global initialized
+    if not initialized:
+        initJokes()
+        initUsers()
+        initCarSpec()
+        initReaders()
+        initialized = True
+
+@app.before_request
 def activate_job():
-    initJokes()
-    initUsers()
-    initCarSpec()
+    initialize_once()
 
 # this runs the application on the development server
 if __name__ == "__main__":
-    # change name for testing
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///volumes/sqlite.db'
-    app.run(debug=True, host="0.0.0.0", port="8087")
+    from flask_cors import CORS
+    cors = CORS(app)
+    app.run(debug=True, host="0.0.0.0", port="8088")
+
+
+
+
+
